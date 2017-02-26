662766c25501ddc2e4f8a95b91051b7356d44da9
Roman Peshkov <peshkovroman@gmail.com>
2017-02-26 22:25:22 +0100

    Added CORS

--- main.go
+++ main.go
@@ -3,9 +3,10 @@ package main
 import (
 	"log"
 	"net/http"
+    "github.com/gorilla/handlers"
 )
 
 func main() {
 	router := NewRouter()
-	log.Fatal(http.ListenAndServe(":8080", router))
+	log.Fatal(http.ListenAndServe(":8080", handlers.CORS()(router)))
 }
